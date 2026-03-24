cc8261361d33c007ff1bc1985f8b55e38e8b4609
Mariana <mariana@MacBook-Air-de-Mariana.local>
2026-03-24 22:31:33 +0100

    Configure logger on main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
+import logging
+
 from scripts.pipelines import run_pipeline
 
 
 if __name__ == "__main__":
+   
+   logging.basicConfig(
+      level=logging.INFO,
+      format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+   )
+
+   logger = logging.getLogger(__name__)
          
    run_pipeline()
     
